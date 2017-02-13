f3925900d6ed7abde811a2600964388b49278380
zeligzhou <zeligzhou@gmail.com>
2017-02-13 19:36:37 +0800

    drag example

--- js/main.js
+++ js/main.js
@@ -40,6 +40,25 @@ Vue.component('card-item2',{
     }
   }
 });
+
+
+Vue.component('section4',{
+  template:'<section><h2>{{title}}</h2><div>{{message}}</div></section>',
+  data:function(){
+    return {
+      title:'示例4：引用js插件',
+      message:'引用jquery及其插件'
+    }
+  }
+});
+Vue.component('card-item3',{
+  template:'<li>{{title}}</li>',
+  props:['title']
+});
+Vue.component('card-item4',{
+  template:'<li>{{title}}</li>',
+  props:['title']
+});
 var app =  new Vue({
     el: '#app',
     data: {
@@ -49,6 +68,45 @@ var app =  new Vue({
         {text:'card1'},
         {text:'card2'},
         {text:'card3'}
+      ],
+      varCards1:[
+        {text:'1card1'},
+        {text:'1card2'},
+        {text:'1card3'},
+        {text:'1card4'},
+        {text:'1card5'},
+        {text:'1card6'},
+        {text:'1card7'},
+        {text:'1card8'},
+        {text:'1card9'},
+        {text:'1card10'},
+        {text:'1card11'},
+        {text:'1card12'}
+      ],
+      varCards2:[
+        {text:'2card1'},
+        {text:'2card2'},
+        {text:'2card3'},
+        {text:'2card4'},
+        {text:'2card5'},
+        {text:'2card6'},
+        {text:'2card7'},
+        {text:'2card8'},
+        {text:'2card9'},
+        {text:'2card10'},
+        {text:'2card11'},
+        {text:'2card12'}
       ]
+    },
+    created:function(){
+      console.log("文档生成后，目标li的数量为："+$("#list1 li").length);
+    },
+    mounted:function(){
+      console.log("组件生成后，目标li的数量为："+$("#list1 li").length);
+      $("#list1, #list2").dragsort({ dragSelector: "li", dragBetween: true, dragEnd: saveOrder  });
+    
+        function saveOrder() {
+          console.log("调用拖动事件后，目标li的数量为："+$("#list1 li").length);
+        };
     }
   });
